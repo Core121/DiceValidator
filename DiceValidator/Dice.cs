21e83c86d2cb684a18640b6cb0a7701e3512e7f5
Corey Franco <42871786+Core121@users.noreply.github.com>
2022-12-11 10:41:27 -0500

    Add project files.

---
+++ DiceValidator/Dice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace DiceValidator
+{
+    public class Dice
+    {
+        public int Sides { get; set; }
+        public List<int> Rolls { get; set; }
+
+        public Dice() 
+        {
+            Rolls = new List<int>();
+        }
+
+        public double getAvgRolls()
+        {
+            return Rolls.Count > 0 ? Rolls.Average() : 0;
+        }
+
+        public double getProbability(int value)
+        {
+            // Amount of rolls on a given number over the total rolls in the sample
+            return ((double)Rolls.Count(x => x == value) / Rolls.Count());
+        }
+
+        public double getProbabilityStandard()
+        {
+            // Get probability of rolling any one value in a perfectly weighted die
+            return ((double)1 / Sides);
+        }
+
+        public int getMode()
+        {
+            if (Rolls.Count > 0)
+            {
+                // Group by each number and return the largest group
+                return Rolls.GroupBy(v => v)
+                .OrderByDescending(g => g.Count())
+                .First().Key;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double GetMedian()
+        {
+            if (Rolls.Count > 0)
+            {
+                var ys = Rolls.OrderBy(x => x).ToList();
+                double mid = (ys.Count - 1) / 2.0;
+                return (ys[(int)(mid)] + ys[(int)(mid + 0.5)]) / 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ DiceValidator/Program.cs
@@ -0,0 +1,98 @@
+﻿using DiceValidator;
+
+Console.WriteLine("Hello and welcome to the dice validator!\n\n");
+
+Dice dice = new();
+dice.Sides= GetDieSides();
+int numberOfRolls = RollCountEntry();
+
+Console.WriteLine("\n\nPlease roll your die and enter the value shown on the dice.\nRepeat until all rolls have been completed or click enter to stop.");
+for (int i = 1; i <= numberOfRolls; i++)
+{
+    int roll = RollEntry();
+    if(roll == -1)
+    {
+        break;
+    }
+    dice.Rolls.Add(roll);
+
+    if (i % 10 == 0 && i != numberOfRolls)
+    {
+        Console.WriteLine((numberOfRolls - i) + " rolls left.");
+    }
+}
+
+Console.WriteLine("Rolls completed. Ouputting Stats");
+
+Console.WriteLine("Mean: " + dice.getAvgRolls().ToString("N3"));
+Console.WriteLine("Median: " + dice.GetMedian());
+Console.WriteLine("Mode: " + dice.getMode());
+
+Console.WriteLine("\n\nStandard Probability of rolling any one value on this die: " + dice.getProbabilityStandard());
+Console.WriteLine("Probability of rolling (from sample): ");
+for(int i = 1; i <= dice.Sides; i++)
+{
+    Console.WriteLine(i + ":\t" + dice.getProbability(i).ToString("N4"));
+}
+Console.WriteLine("Press enter twice to exit");
+Console.ReadLine();
+Console.ReadLine();
+
+int GetDieSides()
+{
+    int sides;
+    bool sidesValidated = false;
+    do
+    {
+        Console.WriteLine("Please enter the number of sides of your die.");
+        var unSanitizedSides = Console.ReadLine();
+        sidesValidated = int.TryParse(unSanitizedSides, out sides) && sides > 0;
+    } while (!sidesValidated);
+    return sides;
+}
+
+int RollCountEntry()
+{
+    int rolls = 100;
+    bool rollsValidated = false;
+    do
+    {
+        Console.WriteLine("Please enter the number of rolls you will be performing or click enter for 100.");
+        var unSanitizedRolls = Console.ReadLine();
+        if (unSanitizedRolls != String.Empty)
+        {
+            rollsValidated = int.TryParse(unSanitizedRolls, out rolls) && rolls > 0;
+        }
+        else
+        {
+            rollsValidated = true;
+        }
+    } while (!rollsValidated);
+    return rolls;
+}
+
+int RollEntry()
+{
+    int roll;
+    bool rollValidated = false;
+    do
+    {
+        var unSanitizedRoll = Console.ReadLine();
+        if (unSanitizedRoll != String.Empty)
+        {
+            rollValidated = int.TryParse(unSanitizedRoll, out roll) && roll <= dice.Sides && roll > 0;
+        }
+        else
+        {
+            roll = -1;
+            rollValidated = true;
+        }
+        if (!rollValidated)
+        {
+            Console.WriteLine("Invalid entry. Please try again.");
+        }
+    } while (!rollValidated);
+    return roll;
+}
+
+
